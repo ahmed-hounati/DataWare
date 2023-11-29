a481b86473048f706adac05229e44fa654c61ec1
ahmed-hounati <hounatiahmed@gmail.com>
2023-11-29 09:13:10 +0100

    remove sign link

--- index.php
+++ index.php
@@ -187,7 +187,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
                                     <input id="remember-me" name="remember-me" type="checkbox" class="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded">
                                     <label for="remember-me" class="ml-2 block text-sm text-gray-900"> Remember me </label>
                                 </div>
-                                <a href="./signup.php" class="text-indigo-600">Sign Up</a>
+                                <!-- <a href="./signup.php" class="text-indigo-600">Sign Up</a> -->
                             </div>
 
                             <div>
