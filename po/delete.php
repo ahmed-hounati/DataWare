88b8662e10f9a3418016a2fab942e6bbcad6f439
ahmed-hounati <hounatiahmed@gmail.com>
2023-12-07 10:12:15 +0100

    update

--- po/delete.php
+++ po/delete.php
@@ -1,21 +1,15 @@
-<?php 
-    // include 'connexion.php';
-    $server="localhost";
-    $nomBD="DataWare";
-    $login="root";
-    $pass="";
+<?php
+require '../includes/conn.inc.php';
 
-    $conn = mysqli_connect($server, $login, $pass, $nomBD);
+if (isset($_GET['DeleteID'])) {
+    $id = $_GET['DeleteID'];
 
-    if (isset($_GET['DeleteID'])){
-        $id=$_GET['DeleteID'];
-
-        $sql = "DELETE FROM perssonel WHERE Id= '$id'";
-        $result = mysqli_query($conn,$sql);
-        if ($result) {
-            header("Location: ./dashboardpo.php");
-            exit();
-        }else{
-            die(mysqli_connect_error($conn));
-        }
+    $sql = "DELETE FROM perssonel WHERE Id= '$id'";
+    $result = mysqli_query($conn, $sql);
+    if ($result) {
+        header("Location: ./dashboardpo.php");
+        exit();
+    } else {
+        die(mysqli_connect_error($conn));
     }
+}
